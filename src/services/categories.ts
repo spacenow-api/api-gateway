7a721fbd78acd9bbd65083cf3bf837f87618f725
Arthemus <arthemus.moreira@gmail.com>
2019-07-03 15:00:06 +1000

    Defining legacy categories endpoint.

--- src/services/categories.ts
+++ src/services/categories.ts
@@ -3,66 +3,28 @@ import { AxiosInstance } from 'axios';
 
 import apiAdapter from '../helpers/adapter/apiAdapter';
 
+const GET_ENDPOINTS = ['/categories/legacy'];
+
 class Categories {
   private api: AxiosInstance;
-
-  private base_url: string;
-
-  private path = '/categories';
 
   private router = Router();
 
   constructor(apiHost: string) {
-    this.base_url = apiHost;
-    this.api = apiAdapter(this.base_url);
+    this.api = apiAdapter(apiHost);
     this.intializeRoutes();
   }
 
   private intializeRoutes() {
-    this.router.get(`${this.path}`, this.getRootCategories);
-    this.router.get(`${this.path}/:id`, this.getCategory);
-    this.router.post(`${this.path}`, this.createCategory);
-    this.router.patch(`${this.path}`, this.createCategory);
+    GET_ENDPOINTS.forEach(o => {
+      this.router.get(o, (req: Request, res: Response, next: NextFunction) => {
+        this.api
+          .get(req.path)
+          .then(result => res.send(result.data))
+          .catch(err => next(err));
+      });
+    });
   }
-
-  private getRootCategories = async (
-    request: Request,
-    response: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const resp = await this.api.get(request.path);
-      response.send(resp.data);
-    } catch (error) {
-      next(error.response.data);
-    }
-  };
-
-  private getCategory = async (
-    request: Request,
-    response: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const resp = await this.api.get(request.path);
-      response.send(resp.data);
-    } catch (error) {
-      next(error.response.data);
-    }
-  };
-
-  private createCategory = async (
-    request: Request,
-    response: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const resp = await this.api.post(request.path, request.body);
-      response.send(resp.data);
-    } catch (error) {
-      next(error.response.data);
-    }
-  };
 }
 
 export default Categories;
