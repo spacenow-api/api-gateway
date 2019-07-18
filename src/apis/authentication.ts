4136d294986a7daca34eff46e9c7244487ee413a
Arthemus <arthemus.moreira@gmail.com>
2019-07-18 10:35:24 +1000

    Token validation.

--- src/apis/authentication.ts
+++ src/apis/authentication.ts
@@ -4,6 +4,7 @@ import { AxiosInstance } from 'axios';
 import apiAdapter from '../helpers/adapter/apiAdapter';
 
 class Authentication {
+
   private api: AxiosInstance;
 
   private base_url: string;
@@ -21,33 +22,33 @@ class Authentication {
   private intializeRoutes() {
     this.router.post(`${this.path}/register`, this.register);
     this.router.post(`${this.path}/signin`, this.signin);
+    this.router.post(`${this.path}/token/validate`, this.tokenValidate);
   }
 
-  private register = async (
-    request: Request,
-    response: Response,
-    next: NextFunction
-  ) => {
+  private register = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const resp = await this.api.post(request.path, request.body);
-      response.send(resp.data);
-    } catch (error) {
-      next(error.response.data);
+      const resp = await this.api.post(req.path, req.body);
+      res.send(resp.data);
+    } catch (err) {
+      next(err.response.data);
     }
   };
 
-  private signin = async (
-    request: Request,
-    response: Response,
-    next: NextFunction
-  ) => {
+  private signin = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const resp = await this.api.post(request.path, request.body);
-      response.send(resp.data);
-    } catch (error) {
-      next(error.response.data);
+      const resp = await this.api.post(req.path, req.body);
+      res.send(resp.data);
+    } catch (err) {
+      next(err.response.data);
     }
   };
+
+  private tokenValidate = async (req: Request, res: Response, next: NextFunction) => {
+    this.api
+      .post(req.path, req.body)
+      .then(result => res.send(result.data))
+      .catch(err => res.status(400).send(err.response.data.message));
+  }
 }
 
 export default Authentication;
